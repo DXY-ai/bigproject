13ab589d4d3d488564a4e6bb1834589b19779a8e
86155 <15589191112@163.com>
2021-01-04 14:05:52 +0800

    hhh

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+      System.out.println(findInfoById("DXY"));
+    }
+
+    public static String findInfoById(String a){
+
+        return  a+"像个傻逼";
+    }
+}
